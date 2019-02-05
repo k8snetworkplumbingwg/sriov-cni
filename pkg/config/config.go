0ab1d2cf6686de62f2ca7b5b0f5475d2ccc6a80b
Piotr Mossakowski <piotrx.mossakowski@intel.com>
2019-02-05 14:16:24 +0000

    Added tests for sriov.go
    This change adds tests (part of them) for sriov.go.
    Test filesystem (testing.go) now uses chroot.
    This was needed to overcome limitations for sriov.go functions
    that invoked utils.go functions.
    
    Using chroot causes another issue with creating new namespace.
    This needs further refactoring - until then blocked tests
    have been disabled (pending in ginkgo).
    
    Refactored sriov.go and config.go to simplify netlink testing.
    
    Implements: MSP-12413
    
    Change-Id: Ie00651972b250f419a914e69c26d8398abae38a4
    Signed-off-by: Piotr Mossakowski <piotrx.mossakowski@intel.com>

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -3,31 +3,10 @@ package config
 import (
 	"encoding/json"
 	"fmt"
-	"os"
 
 	sriovtypes "github.com/intel/sriov-cni/pkg/types"
 	"github.com/intel/sriov-cni/pkg/utils"
-	"github.com/vishvananda/netlink"
 )
-
-// mocked netlink interface
-// required for unit tests
-var nLink NetlinkManager
-
-// NetlinkManager is an interface to get link by name
-type NetlinkManager interface {
-	LinkByName(string) (netlink.Link, error)
-}
-
-// MyNetlink NetlinkManager
-type MyNetlink struct {
-	lm NetlinkManager
-}
-
-// LinkByName implements NetlinkManager
-func (n *MyNetlink) LinkByName(name string) (netlink.Link, error) {
-	return netlink.LinkByName(name)
-}
 
 var (
 	defaultCNIDir = "/var/lib/cni/sriov"
@@ -82,75 +61,4 @@ func getVfInfo(vfPci string) (*sriovtypes.VfInformation, error) {
 		Pfname:  pf,
 		Vfid:    vfID,
 	}, nil
-}
-
-// AssignFreeVF takes in a NetConf object and updates it with an self allocated VF information
-func AssignFreeVF(conf *sriovtypes.NetConf) error {
-	var vfIdx int
-	var infos []string
-	var pciAddr string
-	pfName := conf.Master
-
-	_, err := nLink.LinkByName(pfName)
-	if err != nil {
-		return fmt.Errorf("failed to lookup master %q: %v", conf.Master, err)
-	}
-
-	// get the ifname sriov vf num
-	vfTotal, err := utils.GetSriovNumVfs(pfName)
-	if err != nil {
-		return err
-	}
-
-	if vfTotal <= 0 {
-		return fmt.Errorf("no virtual function in the device %s", pfName)
-	}
-
-	// Select a free VF
-	for vf := 0; vf < vfTotal; vf++ {
-		infos, err = utils.GetVFLinkNames(pfName, vf)
-		if err != nil {
-			if _, ok := err.(*os.PathError); ok {
-				continue
-			} else {
-				return fmt.Errorf("failed to read the virtfn%d dir of the device %q: %v", vf, pfName, err)
-			}
-
-		} else if len(infos) > 0 {
-
-			if len(infos) == MaxSharedVf {
-				conf.Sharedvf = true
-			}
-
-			if len(infos) <= MaxSharedVf {
-				vfIdx = vf
-				pciAddr, err = utils.GetPciAddress(pfName, vfIdx)
-				if err != nil {
-					return fmt.Errorf("err in getting pci address for VF %d of PF %s: %q", vf, pfName, err)
-				}
-				break
-			} else {
-				return fmt.Errorf("multiple network devices found with VF id: %d under PF %s: %+v", vf, pfName, infos)
-			}
-		}
-	}
-
-	if len(infos) == 0 {
-		return fmt.Errorf("no virtual network resources available for the %q", conf.Master)
-	}
-
-	// instantiate DeviceInfo
-	if pciAddr != "" {
-		vfInfo := &sriovtypes.VfInformation{
-			PCIaddr: pciAddr,
-			Pfname:  pfName,
-			Vfid:    vfIdx,
-		}
-		conf.DeviceInfo = vfInfo
-	}
-	return nil
-}
-
-func init() {
-	nLink = &MyNetlink{}
 }
---
+++ pkg/sriov/sriov_suite_test.go
@@ -0,0 +1,31 @@
+package sriov
+
+import (
+	"github.com/intel/sriov-cni/pkg/utils"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+
+	"testing"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+func TestConfig(t *testing.T) {
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Sriov Suite")
+}
+
+var _ = BeforeSuite(func() {
+	// create test sys tree
+	err := utils.CreateTmpSysFs()
+	check(err)
+})
+
+var _ = AfterSuite(func() {
+	var err error
+	err = utils.RemoveTmpSysFs()
+	check(err)
+})
