70a10af696586ccbe3e2a0e623a6bab42dea9e75
Zenghui Shi <zshi@redhat.com>
2019-10-10 21:01:08 +0800

    support CNI RuntimeConfig for Mac
    CNI RuntimeConfig takes preference if configured,
    envArgs can still be used for Mac configuration if
    meta plugin(Multus) passes Mac as envArgs.
    
    Fixes #104

--- pkg/types/types.go
+++ pkg/types/types.go
@@ -7,19 +7,22 @@ import (
 // NetConf extends types.NetConf for sriov-cni
 type NetConf struct {
 	types.NetConf
-	DPDKMode     bool
-	Master       string
-	MAC          string
-	AdminMAC     string `json:"adminMAC"`
-	EffectiveMAC string `json:"effectiveMAC"`
-	Vlan         int    `json:"vlan"`
-	VlanQoS      int    `json:"vlanQoS"`
-	DeviceID     string `json:"deviceID"` // PCI address of a VF in valid sysfs format
-	VFID         int
-	HostIFNames  string // VF netdevice name(s)
-	ContIFNames  string // VF names after in the container; used during deletion
-	MaxTxRate    *int   `json:"max_tx_rate"`          // Mbps, 0 = disable rate limiting
-	SpoofChk     string `json:"spoofchk,omitempty"`   // on|off
-	Trust        string `json:"trust,omitempty"`      // on|off
-	LinkState    string `json:"link_state,omitempty"` // auto|enable|disable
+	DPDKMode      bool
+	Master        string
+	MAC           string
+	AdminMAC      string `json:"adminMAC"`
+	EffectiveMAC  string `json:"effectiveMAC"`
+	Vlan          int    `json:"vlan"`
+	VlanQoS       int    `json:"vlanQoS"`
+	DeviceID      string `json:"deviceID"` // PCI address of a VF in valid sysfs format
+	VFID          int
+	HostIFNames   string // VF netdevice name(s)
+	ContIFNames   string // VF names after in the container; used during deletion
+	MaxTxRate     *int   `json:"max_tx_rate"`          // Mbps, 0 = disable rate limiting
+	SpoofChk      string `json:"spoofchk,omitempty"`   // on|off
+	Trust         string `json:"trust,omitempty"`      // on|off
+	LinkState     string `json:"link_state,omitempty"` // auto|enable|disable
+	RuntimeConfig struct {
+		Mac string `json:"mac,omitempty"`
+	} `json:"runtimeConfig,omitempty"`
 }
