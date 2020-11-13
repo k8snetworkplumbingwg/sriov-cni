d5b682999eb6ef794825f3cb1fa0d75756ee9389
Adrian Moreno <amorenoz@redhat.com>
2020-11-13 10:46:26 +0100

    Treat vhost-vdpa devices as DPDK devices
    Use the govdpa library to inspect the device and determine if it's a
    vdpa device and its driver.
    
    If it's a vhost device, the CNI should not try to do any netdevice
    configuration.
    
    Signed-off-by: Adrian Moreno <amorenoz@redhat.com>

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -8,6 +8,8 @@ import (
 	"path/filepath"
 	"strconv"
 	"strings"
+
+	"github.com/redhat-virtio-net/govdpa/pkg/kvdpa"
 )
 
 var (
@@ -204,6 +206,15 @@ func HasDpdkDriver(pciAddr string) (bool, error) {
 			return true, nil
 		}
 	}
+
+	/* If there is a vdpa device associated with the device and it is bound to
+	vhost-vdpa driver, it shall be treated as a dpdk device
+	*/
+	if vdpaDev, err := kvdpa.GetVdpaDeviceByPci(pciAddr); err != nil &&
+		vdpaDev.GetDriver() == kvdpa.VhostVdpaDriver {
+		return true, nil
+	}
+
 	return false, nil
 }
 
