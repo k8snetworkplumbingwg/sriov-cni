fc25698b84d676c30b111f55eaed670f6b39cd96
Andrea Panattoni <apanatto@redhat.com>
2024-05-31 11:05:01 +0200

    Unit test for `WaitForCarrier`
    Co-authored-by: Thomas Haller <thaller@redhat.com>
    Signed-off-by: Thomas Haller <thaller@redhat.com>

---
+++ pkg/utils/packet_test.go
@@ -0,0 +1,52 @@
+package utils
+
+import (
+	"sync/atomic"
+	"time"
+
+	mocks_utils "github.com/k8snetworkplumbingwg/sriov-cni/pkg/utils/mocks"
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+	"github.com/stretchr/testify/mock"
+
+	"github.com/vishvananda/netlink"
+	"golang.org/x/sys/unix"
+)
+
+var _ = Describe("Packets", func() {
+
+	Context("WaitForCarrier", func() {
+		It("should wait until the link has IFF_UP flag", func() {
+			DeferCleanup(func(old NetlinkManager) { netLinkLib = old }, netLinkLib)
+
+			mockedNetLink := &mocks_utils.NetlinkManager{}
+			netLinkLib = mockedNetLink
+
+			rawFlagsAtomic := new(uint32)
+			*rawFlagsAtomic = unix.IFF_UP
+
+			fakeLink := &FakeLink{LinkAttrs: netlink.LinkAttrs{
+				Index:    1000,
+				Name:     "dummylink",
+				RawFlags: atomic.LoadUint32(rawFlagsAtomic),
+			}}
+
+			mockedNetLink.On("LinkByName", "dummylink").Return(fakeLink, nil).Run(func(args mock.Arguments) {
+				fakeLink.RawFlags = atomic.LoadUint32(rawFlagsAtomic)
+			})
+
+			hasCarrier := make(chan bool)
+			go func() {
+				hasCarrier <- WaitForCarrier("dummylink", 5*time.Second)
+			}()
+
+			Consistently(hasCarrier, "100ms").ShouldNot(Receive())
+
+			go func() {
+				atomic.StoreUint32(rawFlagsAtomic, unix.IFF_UP|unix.IFF_RUNNING)
+			}()
+
+			Eventually(hasCarrier, "300ms").Should(Receive())
+		})
+	})
+})
