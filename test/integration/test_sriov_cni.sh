28507e0a30afc16c4cfbd1e750ff2d092cfb4194
Andrea Panattoni <apanatto@redhat.com>
2025-03-31 18:46:33 +0200

    Integration test for `pull/316`
    Add an integration test to verify that if an application edits the MTU of a VF, then the sriov-cni DEL command
    restore it to the previous value.
    
    Signed-off-by: Andrea Panattoni <apanatto@redhat.com>

--- test/integration/test_sriov_cni.sh
+++ test/integration/test_sriov_cni.sh
@@ -64,3 +64,43 @@ EOM
   assert 'ip netns exec test_root_ns ip link show enp175s6'
   assert_file_contains "${DEFAULT_CNI_DIR}/enp175s0f1.calls" "LinkSetVfVlanQosProto enp175s0f1 0 0 0 33024"
 }
+
+
+test_mtu_reset() {
+
+  create_network_ns "container_1"
+
+  assert 'ip netns exec test_root_ns ip link set mtu 3333 dev enp175s6'
+
+  export CNI_IFNAME=net1
+
+  read -r -d '' CNI_INPUT <<- EOM
+  {
+    "type": "sriov",
+    "cniVersion": "0.3.1",
+    "name": "sriov-network",
+    "vlan": 1234,
+    "ipam": {
+      "type": "test-ipam-cni"
+    },
+    "deviceID": "0000:af:06.0",
+    "mac": "60:00:00:00:00:E1",
+    "logFile": "${DEFAULT_CNI_DIR}/sriov.log",
+    "logLevel": "debug"
+  }
+EOM
+
+  export CNI_COMMAND=ADD
+  assert invoke_sriov_cni
+
+  # Verify the VF has the correct MTU inside the container
+  assert 'ip netns exec container_1 ip link | grep -i 3333'
+
+  # Simulate an application modifying the MTU value
+  assert 'ip netns exec container_1 ip link set mtu 4444 dev net1'
+  assert 'ip netns exec container_1 ip link | grep -i 4444'
+
+  export CNI_COMMAND=DEL
+  assert invoke_sriov_cni
+  assert 'ip netns exec test_root_ns ip link show enp175s6 | grep 3333'
+}
