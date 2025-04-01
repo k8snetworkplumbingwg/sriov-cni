33cb264ed8c4e00af0b342034b70d1b7e8ed6c7a
Andrea Panattoni <apanatto@redhat.com>
2025-04-01 14:04:08 +0200

    integration-test: Archive `DEFAULT_CNI_DIR`
    Signed-off-by: Andrea Panattoni <apanatto@redhat.com>

--- test/integration/libtest.sh
+++ test/integration/libtest.sh
@@ -5,7 +5,7 @@ export CNI_PATH="${this_folder}/test_utils"
 export CNI_CONTAINERID=stub_container
 
 setup() {
-  ip netns del test_root_ns || true
+  ip netns del test_root_ns 2> /dev/null || true
   ip netns add test_root_ns
 
   # See pkg/utils/testing.go
@@ -13,13 +13,17 @@ setup() {
   ip netns exec test_root_ns ip link add enp175s6 type dummy
   ip netns exec test_root_ns ip link add enp175s7 type dummy
 
-  DEFAULT_CNI_DIR=$(mktemp -d)
+  DEFAULT_CNI_DIR=$(mktemp -d "${this_folder}/tmp/default_cni_dir.XXXXX")
   export DEFAULT_CNI_DIR
 }
 
 teardown() {
+  if [ -n "${INT_TEST_SKIP_CLEANUP}" ]; then
+    return
+  fi
+
   # Double check the variable points to something created by the setup() function.
-  if [[ $DEFAULT_CNI_DIR == "/tmp/tmp."* ]]; then
+  if [[ $DEFAULT_CNI_DIR == *"tmp/default_cni_dir."* ]]; then
     rm -rf "$DEFAULT_CNI_DIR"
   fi
 }
