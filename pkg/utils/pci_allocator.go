33db82fe70052761447ec07278d1e41f05470578
Sebastian Sch <sebassch@gmail.com>
2025-12-28 10:01:04 +0000

    multiple bumps
    golangci-lint v2, fix Dockerfile Go version, fix lint issues
    
    Add error logging when failing to close network namespace
    
    Signed-off-by: Sebastian Sch <sebassch@gmail.com>

--- pkg/utils/pci_allocator.go
+++ pkg/utils/pci_allocator.go
@@ -3,7 +3,6 @@ package utils
 import (
 	"fmt"
 	"os"
-	"path"
 	"path/filepath"
 	"time"
 
@@ -33,19 +32,19 @@ func NewPCIAllocator(dataDir string) *PCIAllocator {
 
 // Lock gets an exclusive lock on the given PCI address, ensuring there is no other process configuring / or de-configuring the same device.
 func (p *PCIAllocator) Lock(pciAddress string) error {
-	lockDir := path.Join(p.dataDir, "vf_lock")
-	if err := os.MkdirAll(lockDir, 0600); err != nil {
+	lockDir := filepath.Join(p.dataDir, "vf_lock")
+	if err := os.MkdirAll(lockDir, 0o600); err != nil {
 		return fmt.Errorf("failed to create the sriov lock directory(%q): %v", lockDir, err)
 	}
 
-	path := filepath.Join(lockDir, fmt.Sprintf("%s.lock", pciAddress))
+	lockPath := filepath.Join(lockDir, fmt.Sprintf("%s.lock", pciAddress))
 
 	// unix.O_CREAT - Create the file if it doesn't exist
 	// unix.O_RDONLY - Open the file for read
 	// unix.O_CLOEXEC - Automatically close the file on exit. This is useful to keep the flock until the process exits
-	fd, err := unix.Open(path, unix.O_CREAT|unix.O_RDONLY|unix.O_CLOEXEC, 0600)
+	fd, err := unix.Open(lockPath, unix.O_CREAT|unix.O_RDONLY|unix.O_CLOEXEC, 0o600)
 	if err != nil {
-		return fmt.Errorf("failed to open PCI file [%s] for locking: %w", path, err)
+		return fmt.Errorf("failed to open PCI file [%s] for locking: %w", lockPath, err)
 	}
 
 	errCh := make(chan error)
@@ -57,26 +56,26 @@ func (p *PCIAllocator) Lock(pciAddress string) error {
 	select {
 	case err = <-errCh:
 		if err != nil {
-			return fmt.Errorf("failed to flock PCI file [%s]: %w", path, err)
+			return fmt.Errorf("failed to flock PCI file [%s]: %w", lockPath, err)
 		}
 		return nil
 
 	case <-time.After(pciLockAcquireTimeout):
-		return fmt.Errorf("time out while waiting to acquire exclusive lock on [%s]", path)
+		return fmt.Errorf("time out while waiting to acquire exclusive lock on [%s]", lockPath)
 	}
 }
 
 // SaveAllocatedPCI creates a file with the pci address as a name and the network namespace as the content
 // return error if the file was not created
-func (p *PCIAllocator) SaveAllocatedPCI(pciAddress, ns string) error {
-	if err := os.MkdirAll(p.dataDir, 0600); err != nil {
+func (p *PCIAllocator) SaveAllocatedPCI(pciAddress, netNS string) error {
+	if err := os.MkdirAll(p.dataDir, 0o600); err != nil {
 		return fmt.Errorf("failed to create the sriov data directory(%q): %v", p.dataDir, err)
 	}
 
-	path := filepath.Join(p.dataDir, pciAddress)
-	err := os.WriteFile(path, []byte(ns), 0600)
+	pciPath := filepath.Join(p.dataDir, pciAddress)
+	err := os.WriteFile(pciPath, []byte(netNS), 0o600)
 	if err != nil {
-		return fmt.Errorf("failed to write used PCI address lock file in the path(%q): %v", path, err)
+		return fmt.Errorf("failed to write used PCI address lock file in the path(%q): %v", pciPath, err)
 	}
 
 	return err
@@ -85,9 +84,9 @@ func (p *PCIAllocator) SaveAllocatedPCI(pciAddress, ns string) error {
 // DeleteAllocatedPCI Remove the allocated PCI file
 // return error if the file doesn't exist
 func (p *PCIAllocator) DeleteAllocatedPCI(pciAddress string) error {
-	path := filepath.Join(p.dataDir, pciAddress)
-	if err := os.Remove(path); err != nil {
-		return fmt.Errorf("error removing PCI address lock file %s: %v", path, err)
+	pciPath := filepath.Join(p.dataDir, pciAddress)
+	if err := os.Remove(pciPath); err != nil {
+		return fmt.Errorf("error removing PCI address lock file %s: %v", pciPath, err)
 	}
 	return nil
 }
@@ -96,19 +95,19 @@ func (p *PCIAllocator) DeleteAllocatedPCI(pciAddress string) error {
 // if it exists we also check the network namespace still exist if not we delete the allocation
 // The function will return an error if the pci is still allocated to a running pod
 func (p *PCIAllocator) IsAllocated(pciAddress string) (bool, error) {
-	path := filepath.Join(p.dataDir, pciAddress)
-	_, err := os.Stat(path)
+	pciPath := filepath.Join(p.dataDir, pciAddress)
+	_, err := os.Stat(pciPath)
 	if err != nil {
 		if os.IsNotExist(err) {
 			return false, nil
 		}
 
-		return false, fmt.Errorf("failed to check for pci address file for %s: %v", path, err)
+		return false, fmt.Errorf("failed to check for pci address file for %s: %v", pciPath, err)
 	}
 
-	dat, err := os.ReadFile(path)
+	dat, err := os.ReadFile(pciPath) //nolint:gosec
 	if err != nil {
-		return false, fmt.Errorf("failed to read for pci address file for %s: %v", path, err)
+		return false, fmt.Errorf("failed to read for pci address file for %s: %v", pciPath, err)
 	}
 
 	// To prevent a locking of a PCI address for every pciAddress file we also add the netns path where it's been used
@@ -128,6 +127,10 @@ func (p *PCIAllocator) IsAllocated(pciAddress string) (bool, error) {
 	}
 
 	// Close the network namespace
-	networkNamespace.Close()
+	if err := networkNamespace.Close(); err != nil {
+		logging.Error("Failed to close network namespace",
+			"namespace", string(dat),
+			"error", err)
+	}
 	return true, nil
 }
